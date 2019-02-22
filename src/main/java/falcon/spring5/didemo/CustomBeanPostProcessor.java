b189db8dbaeef7660a05221a57f5897db5ed7663
MMrFalcon <35698172+MMrFalcon@users.noreply.github.com>
2019-02-22 17:06:05 +0100

    Bean Life Cycle tests

---
+++ src/main/java/falcon/spring5/didemo/CustomBeanPostProcessor.java
@@ -0,0 +1,27 @@
+package falcon.spring5.didemo;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+
+        if(bean instanceof BeanLifeCycle){
+            ((BeanLifeCycle) bean).beforeInit();
+        }
+
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if(bean instanceof BeanLifeCycle){
+            ((BeanLifeCycle) bean).afterInit();
+        }
+
+        return bean;
+    }
+}
